ea81c745fc9c42989490d2bb4f181eae9e7d314b
ralph.francesconi007 <ralph.francesconi007@gmail.com>
2021-02-05 13:59:09 -0500

    Fixed the modal bug

--- src/components/ShowPokemon/showPokemon.js
+++ src/components/ShowPokemon/showPokemon.js
@@ -10,7 +10,11 @@ const PokemonIndex = ({ user }) => {
   const [pokemon, setPokemon] = useState([])
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+  const [pokemonObj, setPokemonObj] = useState({})
+  const handleShow = (selectedPokemon) => {
+    setPokemonObj(selectedPokemon)
+    setShow(true)
+  }
   useEffect(() => {
     axios({
       url: `${apiUrl}/pokemon`,
@@ -28,23 +32,9 @@ const PokemonIndex = ({ user }) => {
         <img src={pokemon.pokemonImage} className="pokemon-image" alt="pokemonImage" />
         <Link to={`/pokemon-index/${pokemon._id}`}><h4 className="name-style">Name: {pokemon.name}</h4></Link>
         <h4 className="type-style">Type: {pokemon.type}</h4>
-        <Button variant="primary" onClick={handleShow}>
+        <Button variant="primary" onClick={() => handleShow(pokemon)}>
         See Skills!
         </Button>
-        <div key={pokemon._id}>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title className="modal-title-style">Pokemon Skills</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <h4 className="modal-style">Best Move: {pokemon.move}</h4>
-              <h4 className="modal-style">Strong Against: {pokemon.strengths}</h4>
-              <h4 className="modal-style">Weak Against: {pokemon.weaknesses}</h4>
-            </Modal.Body>
-            <Modal.Footer>
-            </Modal.Footer>
-          </Modal>
-        </div>
       </div>
     )
   })
@@ -54,6 +44,18 @@ const PokemonIndex = ({ user }) => {
       <div className="pokemon-grid">
         {pokemonjsx}
       </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className="modal-title-style">Pokemon Skills</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4 className="modal-style">Best Move: {pokemonObj.move}</h4>
+          <h4 className="modal-style">Strong Against: {pokemonObj.strengths}</h4>
+          <h4 className="modal-style">Weak Against: {pokemonObj.weaknesses}</h4>
+        </Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
